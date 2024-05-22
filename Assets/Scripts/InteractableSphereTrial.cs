c704104870a01e7fcc0fda07e3482f2cae63fc01
Yang Xiang (FESCO Adecco Human Resources) <v-yangxiang@microsoft.com>
2024-05-22 17:40:19 +0800

    feat/integarated interaction system with sphere demo object

---
+++ Assets/Scripts/InteractableSphereTrial.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EIS.Runtime.Core;
+using EIS.Runtime.Core.Hints;
+
+public class InteractableSphereTrial : Interactable
+{
+    // Start is called before the first frame update
+
+
+    public override bool UsePrimaryActionHint() => true;
+    public override string GetPrimaryHintText() => "Interact";
+
+    public override bool UseSecondaryActionHint() => false;
+    public override string GetSecondaryHintText() => default;
+
+    public override bool IsUnsubscribeBlocked() => false;
+
+    // [SerializeField] private InventoryItemData inventoryItemData;
+
+    public override void OnInteractPrimary()
+    {
+        Debug.Log("what can I say");
+        // InventoryController.AddToInventory(inventoryItemData);
+        // Destroy(gameObject);
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
