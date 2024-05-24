b4233761f76f0d4da4912f2d866887fdbcee8a22
Yang Xiang (FESCO Adecco Human Resources) <v-yangxiang@microsoft.com>
2024-05-24 15:37:19 +0800

    feat/add self-rotating gift box

---
+++ Assets/Scripts/SelfRotate.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfRotate : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float speed;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0f, speed * Time.deltaTime ,0f, Space.Self);
+    }
+}
